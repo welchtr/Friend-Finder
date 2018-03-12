996aa5c36e0b08d1bee78bfc4f1703f348e5afd7
Tiara Welch <twelch1586@gmail.com>
2018-03-12 12:13:08 -0400

    made updated changes to deploy to heroku

---
+++ app/data/friend.js
@@ -0,0 +1,54 @@
+var friendsArray = [{
+    name: "Amina Jordan",
+    email: "emily@gmail.com",
+    picture: "http://www.imdb.com/name/nm0000956/mediaviewer/rm218083840",
+    scores: [
+      3,
+      5,
+      4,
+      1,
+      5,
+      2,
+      4,
+      1,
+      5,
+      2
+    ]
+  },
+  {
+    name: "Jessica Rivers",
+    email: "aj@gmail.com",
+    picture: "http://images5.fanpop.com/image/photos/28700000/Rory-Gilmore-rory-gilmore-28791682-1884-2500.jpg",
+    scores: [
+      5,
+      4,
+      3,
+      2,
+      1,
+      5,
+      4,
+      3,
+      2,
+      5
+    ]
+  },
+  {
+    name: "Robert Michael",
+    email: "stephen@gmail.com",
+    picture: "https://vignette.wikia.nocookie.net/rocky/images/f/fd/Michael-B-Jordan.jpg/revision/latest?cb=20160226012927",
+    scores: [
+      4,
+      4,
+      4,
+      4,
+      5,
+      5,
+      4,
+      2,
+      5,
+      2
+    ]
+  }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var bodyParser = require("body-parser");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,8 +31,8 @@ app.use(express.static('app/public'));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
